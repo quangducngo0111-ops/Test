7bc9e47044988073bea1d343a90d9b472da18774
quangducngo0111-ops <quangduc.ngo0111@gmail.com>
2026-09-21 10:54:04 +0700

    Update manifest.js

--- Practice/Vocabulary/data/manifest.js
+++ Practice/Vocabulary/data/manifest.js
@@ -1,8 +1,25 @@
 window.VOCAB_MANIFEST = [
+
+  /* =========================
+     ECONOMY · 6 SETS
+  ========================= */
+
   "./data/topics/economy/01.js",
   "./data/topics/economy/02.js",
   "./data/topics/economy/03.js",
   "./data/topics/economy/04.js",
   "./data/topics/economy/05.js",
-  "./data/topics/economy/06.js"
+  "./data/topics/economy/06.js",
+
+  /* =========================
+     EDUCATION · 6 SETS
+  ========================= */
+
+  "./data/topics/education/01.js",
+  "./data/topics/education/02.js",
+  "./data/topics/education/03.js",
+  "./data/topics/education/04.js",
+  "./data/topics/education/05.js",
+  "./data/topics/education/06.js"
+
 ];
